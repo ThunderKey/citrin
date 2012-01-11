80a9d0342bf46e8c10c5c0a26d12ea3b0a9bd234
Mike <mike@citrin.ch>
2012-01-11 17:14:18 +0100

    help added

--- lib/citrin/cli.rb
+++ lib/citrin/cli.rb
@@ -2,19 +2,10 @@ require 'citrin'
 
 class Citrin::CLI
 
-  def help
-    puts %Q{
-      Usage: citrin command [args]
-      Commands are:
-       database create appname
-       webserver create appname
-       svn create appname
-    }
-  end
 
   def self.start(*args)
     command = args.shift.strip rescue "help"
-    `../commands/#{command} #{args}`
+    `../../commands/#{command} #{args}`
   end
 
 end
