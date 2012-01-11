a57d11f5fc99d3b1a06d48fb0b9d68c3d7602bb3
Mike <mike@citrin.ch>
2012-01-11 17:15:41 +0100

    help added

--- lib/citrin/cli.rb
+++ lib/citrin/cli.rb
@@ -5,7 +5,7 @@ class Citrin::CLI
 
   def self.start(*args)
     command = args.shift.strip rescue "help"
-    `../../commands/#{command} #{args}`
+    `#{$:}../../commands/#{command} #{args}`
   end
 
 end
