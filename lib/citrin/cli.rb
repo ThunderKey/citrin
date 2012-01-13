d3ec58dec1934ee4f46f40a03c567d25ceb9e217
root <root@rh00.citrin.ch>
2012-01-13 17:27:25 +0100

    bugfixes

--- lib/citrin/cli.rb
+++ lib/citrin/cli.rb
@@ -16,14 +16,11 @@ class Citrin::CLI
         name,
         webserver_template_file(env),
         webserver_config_file(name, env),
-        url_for(name, env),
+        url_for(name, env)
       ]
-      puts `#{File.dirname(__FILE__)}/../../commands/create_svn #{args.join(" ")} --with-rails`
+      puts `#{File.dirname(__FILE__)}/../../commands/create_webserver #{args.join(" ")}`
     else
       puts `#{File.dirname(__FILE__)}/../../commands/#{command} #{args.join(" ")}`
-    end
-    if command == 
-    else
     end
   end
 
