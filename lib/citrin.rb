22411496151bc7094bf68f270499ba2b5c82ce55
Mike <mike@citrin.ch>
2012-01-13 14:37:48 +0100

    testing works

--- lib/citrin.rb
+++ lib/citrin.rb
@@ -1,3 +1,4 @@
+
 module Citrin
   def self.do
     "Hello World from the all mighty helloworld Gem!"
--- test/test_citrin.rb
+++ test/test_citrin.rb
@@ -1,11 +1,13 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'helper'
+require 'citrin/cli'
 
 class TestCitrin < Test::Unit::TestCase
   should "create webserver config" do
     appname = "citest"
     Citrin::CLI.start("create_webserver", appname)
-    assert_true File.exists?("/etc/apache2/sites-enabled/prod.#{appname}.conf")
+    assert File.exists?("/etc/apache2/sites-enabled/prod.#{appname}.conf")
     statuscode = `curl -sL -w "%{http_code}" "#{appname}.$(hostname).citrin.ch" -o /dev/null`
-    assert_equals statuscode, "404"
+    assert_equal "404", statuscode
   end
 end
