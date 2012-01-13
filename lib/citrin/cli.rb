77a42eb6b5ceef539f3e45dc7d3cd41d59f34a5f
Mike <mike@citrin.ch>
2012-01-13 17:25:05 +0100

    started adding new functions

--- lib/citrin/cli.rb
+++ lib/citrin/cli.rb
@@ -1,13 +1,29 @@
 require 'citrin'
+require 'citrin_helper'
 
 class Citrin::CLI
+  include CitrinHelper
 
   def self.start(*args)
     command = args.shift.strip rescue "help"
-    if command == "create_svn_rails"
+    case command
+    when "create_svn_rails"
+      puts `#{File.dirname(__FILE__)}/../../commands/create_svn #{args.join(" ")} --with-rails`
+    when "create_webserver"
+      name = args[0]
+      env = args[1]
+      args = [
+        name,
+        webserver_template_file(env),
+        webserver_config_file(name, env),
+        url_for(name, env),
+      ]
       puts `#{File.dirname(__FILE__)}/../../commands/create_svn #{args.join(" ")} --with-rails`
     else
       puts `#{File.dirname(__FILE__)}/../../commands/#{command} #{args.join(" ")}`
+    end
+    if command == 
+    else
     end
   end
 
