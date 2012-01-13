b3a9251a1a304948d2b7a5f4d650e7f7ef635fd4
Mike <mike@citrin.ch>
2012-01-13 15:53:03 +0100

    tests improved

--- test/test_citrin.rb
+++ test/test_citrin.rb
@@ -3,11 +3,21 @@ require 'helper'
 require 'citrin/cli'
 
 class TestCitrin < Test::Unit::TestCase
+
+  def setup
+    @appname = "citest" 
+  end
+
+  def cleanup
+    `rm /etc/apache2/sites-enabled/prod.#{@appname}.conf`
+  end
+
   should "create webserver config" do
-    appname = "citest"
-    Citrin::CLI.start("create_webserver", appname)
-    assert File.exists?("/etc/apache2/sites-enabled/prod.#{appname}.conf")
-    statuscode = `curl -sL -w "%{http_code}" "#{appname}.$(hostname).citrin.ch" -o /dev/null`
-    assert_equal "404", statuscode
+    Citrin::CLI.start("create_webserver", @appname)
+    assert File.exists?("/etc/apache2/sites-enabled/prod.#{@appname}.conf")
+    `cd /var/www/rails_apps/prod/ && rails new #{@appname}`
+    statuscode = `curl -sL -w "%{http_code}" "#{@appname}.$(hostname).citrin.ch" -o /dev/null`
+    assert_equal "200", statuscode
+    cleanup
   end
 end
